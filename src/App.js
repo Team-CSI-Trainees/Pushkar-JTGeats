5948f8a9bfca4dedfae9424e23a12b94d1535914
Pushkar Khare <kharepushkar2804@gmail.com>
2022-11-10 23:11:24 +0530

    navLinks

--- src/App.js
+++ src/App.js
@@ -3,32 +3,27 @@ import Landing from './Components/landing';
 import Service from './Components/service';
 import Contact from './Components/contact';
 import Footer from './Components/footer';
-import AddedCartItem from './Components/addedCartItem';
+// import AddedCartItem from './Components/addedCartItem';
 // import NavBar from './Components/navBar';
 // import NavLinks from './Components/navLinks';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 
 
 function App(){
     return <div>
         
-            <BrowserRouter>
+           <BrowserRouter>
                 <Routes>
-                    <Route path="/" element={<Landing />} />
-                    <Route path="/kitchenSection" element={<KitchenSection />} />
-                    <Route path='/service' element={<Service />} />
-                    <Route path='/contact' element={<Contact />} />
-                
+                    <Route path="/" element={Landing} exact />
+                    <Route path="/kitchenSection" element={KitchenSection} exact />
+                    <Route path="/service" element={Service} exact />
+                    <Route path="/contact" element={Contact} exact />
                 </Routes>
-            </BrowserRouter>
-        
-            {/* <NavBar /> */}
-            {/* <Landing /> */}
-            <KitchenSection />
-            <Service />
-            <Contact />
-            <Footer />
-            {/* <AddedCartItem /> */}
+                <Landing />
+                <KitchenSection />
+                <Service />
+                <Footer />
+           </BrowserRouter>
         </div>;
     
 }
--- src/Components/navLinks.js
+++ src/Components/navLinks.js
@@ -1,29 +1,15 @@
-// import Landing from './landing'
-// import KitchenSection from './kitchenSection'
-// import Service from './service'
-// import Contact from './contact'
-// import Footer from './footer'
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React from 'react'
+import { NavLink } from 'react-router-dom'
 
-
-const NavLinks=()=>{
-    return (
-        // <div className='list'>
-        <div>
-            {/* <ul className='list'>
-                    <li><Link to='/'>Home</Link></li>
-                    <li><Link to='/kitchenSection'>About</Link></li>
-                    <li><Link to='/service'>Menu</Link></li>
-                    <li><Link to='/contact'>Contact</Link></li>
-            </ul> */}
-                
-                {/* <Link to='/'><span className='leftMargin'>Home</span></Link>
-                <Link to='/KitchenSection'><span>About</span></Link>
-                <Link to='/Service'><span>Menu</span></Link>
-                <Link to='/Contact'><span className='noRightMargin'>Contact</span></Link> */}
-        </div>
-    );
+const navLinks = () => {
+  return (
+    <div className='list'>
+        <NavLink to='/'><span>Home</span></NavLink>
+        <NavLink to='/kitchenSection'><span>About</span></NavLink>
+        <NavLink to='/service'><span>Menu</span></NavLink>
+        <NavLink to='/contact'><span>Contact</span></NavLink>
+    </div>
+  )
 }
 
-export default NavLinks
+export default navLinks
